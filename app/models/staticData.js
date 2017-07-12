d80b3a12eb9472588aaa8fd7fc4f12000436c99c
Mor Cohen <mor.cohen1989@gmail.com>
2017-07-12 19:25:03 +0300

    add boosted builder
    Added /build command to use boosted builder!

--- app/models/staticData.js
+++ app/models/staticData.js
@@ -3,6 +3,22 @@
 const Promise = require('promise');
 const request = require('request');
 const config = require('../../config/config');
+const _ = require('lodash');
+
+const CHAMPIONS = "champions";
+const ITEMS = "items";
+const MASTERIES = "masteries";
+const RUNES = "runes";
+const SUMMONER_SPELLS = "summoner-spells";
+const region = 'eun1';
+const ERROR = -1;
+const TEEMO_ID = 17;
+module.exports.TEEMO_ID = TEEMO_ID;
+let championsData = {};
+let itemsData = {};
+let masteriesData = {};
+let runesData = {};
+let summonerSpellsData = {};
 
 // Define static functions and objects.
 
@@ -23,54 +39,98 @@ const regionalEndpoints = {
 };
 
 /**
- * Get champions list.
+ * Get static data.
  * @returns {Promise} result
- * @resolves {Object} result.championsObject
+ * @resolves {Object} result.dataObject
  * @rejects {String} result.message
  */
-const loadChampions = () => {
-
+const getStaticDataFromServer = (data) => {
+    console.log(`gathering ${data} data from server...`);
     return new Promise((fulfill, reject) => {
 
         let result = {};
+        let currentStaticDictionary = getDataDict(data);
+        if (currentStaticDictionary === ERROR) {
+            console.log(`could not identify the data you requested : ${data}`);
+            result.message = `could not identify the data you requested : ${data}`;
+            reject(result);
+        } else {
+            let apiAddress = encodeURI(`https://${region}.api.riotgames.com/lol/static-data/v3/${data}?locale=en_US&dataById=false&api_key=${config.riotAPIKey}`);
+            // console.log(apiAddress);
 
-        const apiAddress = encodeURI('https://global.api.riotgames.com/api/lol/static-data/EUW/v1.2/champion?dataById=true&api_key=' + config.riotAPIKey);
-        console.info(apiAddress);
-
-        // Send API request.
-        request(apiAddress, (error, response, body) => {
-            if (error) {
-                console.error(error);
-                result.message = 'Something went wrong :(';
-                reject(result);
-            }
-            else if (response.statusCode == 404) {
-                result.message = 'Something went wrong :(';
-                reject(result);
-            }
-            else if (response.statusCode == 200) {
-
-                const jsonObject = JSON.parse(body);
-                result.championsObject = jsonObject.data;
-                fulfill(result);
-            }
-            else {
-                result.message = 'Something went wrong :(';
-                reject(result);
-            }
-        });
+            request(apiAddress, (error, response, body) => {
+                if (error) {
+                    console.error(error);
+                    result.message = 'Something went wrong :(';
+                    reject(result);
+                }
+                else {
+                    let items = JSON.parse(body).data;
+                    Object.keys(items).forEach((k) => {
+                        currentStaticDictionary[items[k].id] = items[k];
+                    });
+                    fulfill(currentStaticDictionary);
+                }
+            });
+        }
     });
 };
 
+/**
+ * 
+ * @param {*} data the data requested 
+ * @returns the data structure for the requested data
+ */
+const getDataDict = (data) => {
+    switch (data) {
+        case CHAMPIONS: {
+            return championsData;
+        }
+        case ITEMS: {
+            return itemsData;
+        }
+        case MASTERIES: {
+            return masteriesData;
+        }
+        case RUNES: {
+            return runesData;
+        }
+        case SUMMONER_SPELLS: {
+            return summonerSpellsData;
+        }
+        default: {
+            return ERROR;
+        }
+    }
+}
+
 // Load static data.
 
-// Champion object.
-loadChampions().
-    then((result) => {
-        module.exports.championsObject = result.championsObject;
-    }).catch((result) => {
-        module.exports.championsObject = undefined;
+const dataToGet = [
+    getStaticDataFromServer(CHAMPIONS),
+    getStaticDataFromServer(ITEMS),
+    getStaticDataFromServer(MASTERIES),
+    getStaticDataFromServer(RUNES),
+    getStaticDataFromServer(SUMMONER_SPELLS)
+];
+
+//initialization of data
+const init = () => {
+    return new Promise((fulfill, reject) => {
+        Promise.all(dataToGet).then(() => {
+            console.log("Done! got everything I need");
+            console.log();
+            module.exports.championsObject = championsData;
+            module.exports.itemsObject = itemsData;
+            module.exports.masteriesObject = masteriesData;
+            module.exports.runesObject = runesData;
+            module.exports.summonerSpellsObject = summonerSpellsData;
+            fulfill();
+        });
     });
+}
+
+init();
 
 // Regions.
 module.exports.regionalEndpoints = regionalEndpoints;
