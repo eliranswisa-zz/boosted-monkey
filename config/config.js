e6866c096f7fc5fe15b2af8d6008934a71f669ca
Mor Cohen <mor.cohen1989@gmail.com>
2017-07-12 19:29:54 +0300

    remove tokens
    remove tokens

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 'use strict';
 
-module.exports.riotAPIKey = 'RGAPI-b417c8b0-ad35-4200-bf5e-e413d198a6bb';
-module.exports.championGGKey = 'b376fc6e053c273cf2bb2c076d69c06a';
-module.exports.telegramToken = '406553660:AAHZJQG9btZfDIAVwp2j75hz4v4a4m9dYTQ';
+module.exports.riotAPIKey = '<RIOT TOKEN>';
+module.exports.championGGKey = '<CHAMPIONGG TOKEN>';
+module.exports.telegramToken = '<TELEGRAM TOKEN>';
 module.exports.twitchToken = '<TWITCH TOKEN>';
 
 // Users allowed to use the bot. 
